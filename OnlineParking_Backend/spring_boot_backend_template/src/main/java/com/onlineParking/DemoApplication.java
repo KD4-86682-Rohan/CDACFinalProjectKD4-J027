0956e961b928f16596c0ac9fe88b836851f0bcb9
ROhan <indulkarrohan0@gmail.com>
2025-01-27 12:45:26 +0530

    update

--- OnlineParking_Backend/spring_boot_backend_template/src/main/java/com/onlineParking/DemoApplication.java
+++ OnlineParking_Backend/spring_boot_backend_template/src/main/java/com/onlineParking/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.blogs;
+package com.onlineParking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ OnlineParking_Backend/spring_boot_backend_template/src/main/java/com/onlineParking/Pojos/BaseEntity.java
@@ -0,0 +1,5 @@
+package com.onlineParking.Pojos;
+
+public class BaseEntity {
+
+}
