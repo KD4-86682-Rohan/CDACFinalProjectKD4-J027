23841d1439a26415d1fc2cabd84ca93e1a0b2acb
ROhan <indulkarrohan0@gmail.com>
2025-01-30 23:08:05 +0530

    ParkingSlot, SlotAvailability service and controller added

--- OnlineParking_Backend/spring_boot_backend_template/src/main/java/com/onlineParking/Controller/SlotAvailabilityController.java
+++ OnlineParking_Backend/spring_boot_backend_template/src/main/java/com/onlineParking/Controller/SlotAvailabilityController.java
@@ -1,14 +1,56 @@
 package com.onlineParking.Controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.onlineParking.DTO.ParkingSlotRespDto;
+import com.onlineParking.DTO.SlotAvailabilityReqDto;
+import com.onlineParking.Services.SlotAvailabilityService;
+
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/User")
+@RequestMapping("/Availability")
 @CrossOrigin(origins = "http://localhost:3000")
 @Validated
 public class SlotAvailabilityController {
-
+	@Autowired
+	private SlotAvailabilityService slotAvailabilityService;
+	
+	@PostMapping("/addAvailability/{sId}")
+	public ResponseEntity<?> addSlotAvailability(@PathVariable Long sId,@Valid @RequestBody SlotAvailabilityReqDto dto)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(slotAvailabilityService.addNewSlotAvailability(sId, dto));
+	}
+	
+	@GetMapping("/availabilityByDate/{lId}")
+	public ResponseEntity<?> checkAvailabilityByDate(@PathVariable Long lId, @RequestParam("date") LocalDate date) {
+	    List<ParkingSlotRespDto> slots = slotAvailabilityService.findByDate(lId, date);
+	    if (slots.isEmpty()) {
+	        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	    }
+	    return ResponseEntity.ok(slots);
+	}
+	
+	@GetMapping("/availabilityBySlot/{lId}")
+	public ResponseEntity<?> checkAvailabilityBySlot(@PathVariable Long lId)
+	{
+		List<ParkingSlotRespDto> slots = slotAvailabilityService.findByParkingSlot(lId);
+		if(slots.isEmpty())
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		return ResponseEntity.ok(slots);
+	}
 }
--- OnlineParking_Backend/spring_boot_backend_template/src/main/java/com/onlineParking/Services/SlotAvailabilityService.java
+++ OnlineParking_Backend/spring_boot_backend_template/src/main/java/com/onlineParking/Services/SlotAvailabilityService.java
@@ -1,13 +1,16 @@
 package com.onlineParking.Services;
 
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+import com.onlineParking.DTO.ApiResponse;
 import com.onlineParking.DTO.ParkingSlotRespDto;
+import com.onlineParking.DTO.SlotAvailabilityReqDto;
 
 public interface SlotAvailabilityService {
 
-	List<ParkingSlotRespDto> findByDate(Long lId, Date date);
+	List<ParkingSlotRespDto> findByDate(Long lId, LocalDate date);
 	List<ParkingSlotRespDto> findByParkingSlot(Long lId);
+	ApiResponse addNewSlotAvailability(Long sId, SlotAvailabilityReqDto dto);
 }
